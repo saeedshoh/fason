a13ff4dfc8ba6ea93707d5b614ddcdfd748002e8
Doston <doston.adilov@yandex.ru>
2021-01-10 17:23:24 +0500

    AppServiceProvider расскоментировал

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,28 +29,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Paginator::useBootstrap();
-        // if (Schema::hasTable('banners')) {
-        //     $header_banner = Banners::where('type', 2)->where('position', 1)->latest()->first();
-        //     if($header_banner){
-        //         $is_store = null;
-        //         if (Auth::check()) {
-        //             $is_store = $this->stores->where('user_id', Auth::id())->first();
-        //         }
-        //         view()->share(['is_store' => $is_store, 'header_banner' => $header_banner]);
-        //     }
-        // }
+        Paginator::useBootstrap();
+        if (Schema::hasTable('banners')) {
+            $header_banner = Banners::where('type', 2)->where('position', 1)->latest()->first();
+            if($header_banner){
+                $is_store = null;
+                if (Auth::check()) {
+                    $is_store = $this->stores->where('user_id', Auth::id())->first();
+                }
+                view()->share(['is_store' => $is_store, 'header_banner' => $header_banner]);
+            }
+        }
 
-        // view()->composer(
-        //     ['layouts.header','home', 'products.*', 'favorite'],
-        //     function ($view) {
-        //         $is_store = null;
-        //         if (Auth::check()) {
-        //             $is_store = Store::where('user_id', Auth::id())->first();
-        //         }
+        view()->composer(
+            ['layouts.header','home', 'products.*', 'favorite'],
+            function ($view) {
+                $is_store = null;
+                if (Auth::check()) {
+                    $is_store = Store::where('user_id', Auth::id())->first();
+                }
 
-        //         $view->with(['is_store' => $is_store]);
-        //     }
-        // );
+                $view->with(['is_store' => $is_store]);
+            }
+        );
     }
 }
