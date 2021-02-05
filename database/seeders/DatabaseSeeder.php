112b34ea4f3213132f10b1a52b775d35a86a25d5
shuhrat <shuhrat.safoev@colibri.tj>
2021-02-05 16:53:52 +0500

    адаптивность страниц, слайдер продуктов изменен,поля номера типизированы как числовые, фильтр изменен, значки фильтрации добавлены, категории в мобильной версии добавлены и отступы к страницам поставлены

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,21 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Category::factory(20)->create();
-        // Product::factory(20)->create();
-        $this->call([
-            CitySeeder::class,
-            // CategorySeeder::class,
-            // AttributeSeeder::class,
-            // ProductStatusSeeder::class,
-            // BannerSeeder::class,
-            // OrderStatusSeeder::class,
-            UserSeeder::class,
-            // StoreSeeder::class,
-            // ProductSeeder::class,
-            // MonetizationSeeder::class,
-            // PermissionSeeder::class,
-        ]);
+        Category::factory(20)->create();
+        Product::factory(20)->create();
+        // $this->call([
+        //     // CitySeeder::class,
+        //     // CategorySeeder::class,
+        //     // AttributeSeeder::class,
+        //     // ProductStatusSeeder::class,
+        //     // BannerSeeder::class,
+        //     // OrderStatusSeeder::class,
+        //     // UserSeeder::class,
+        //     // StoreSeeder::class,
+        //     // ProductSeeder::class,
+        //     // MonetizationSeeder::class,
+        //     // PermissionSeeder::class,
+        // ]);
         // Product::factory(50)->create();
     }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -20809,6 +20809,50 @@ function _typeof2(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "funct
 var _require = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js"),
     stubString = _require.stubString;
 
+__webpack_require__(/*! ./jquery.inputmask.bundle.js */ "./resources/js/jquery.inputmask.bundle.js"); ////===================aaaaaaaaaaaaaaaaaaaaaaaaaa===================//
+
+
+$(function () {
+  $("#galler").change(function () {
+    var fd = new FormData();
+    fd.append('_token', $('meta[name=csrf-token]').attr("content"));
+    var files = $('#galler')[0].files;
+
+    if (files.length > 0) {
+      for (var i = 0; i < files.length; i++) {
+        fd.append('image', files[i]);
+        $.ajax({
+          url: '/uploadImage',
+          type: 'post',
+          data: fd,
+          contentType: false,
+          processData: false,
+          success: function success(response) {
+            $('#db-preview-image').find('.product_image[data-image="false"]').first().html('').attr('data-image', 'true').append("\n                            <div class=\"profile-pic\">\n                                <img src=\"/storage/".concat(response, "\" data-image-src=\"").concat(response, "\" class=\"position-relative mw-100 pic-item\">\n                                <div class=\"deleteImage\"><i class=\"fa fa-trash fa-lg text-danger\"></i></div>\n                            </div>\n                    "));
+            var gallery = $('#gallery');
+
+            if (gallery.val() == '') {
+              gallery.val(gallery.val() + response);
+            } else {
+              gallery.val(gallery.val() + ',' + response);
+            }
+          }
+        });
+      }
+    } else {
+      alert("Please select a file.");
+    }
+  });
+});
+$('body').on('click', '.deleteImage', function () {
+  var url = $(this).parent().find('img').data('image-src'); // if(url.indexOf('storage/')){
+  //     url = url.split('/storage/')[1]
+  // }
+
+  console.log('url= ' + url);
+  var gallery = $('#gallery');
+  var array = gallery.val().split(',');
+  var index = array.indexOf(url);
 
   if (index > -1) {
     array.splice(index, 1);
@@ -21241,13 +21285,15 @@ $('#btn-login, #code').on('click change', function () {
       code: code
     },
     success: function success(data) {
-      if ($('#adressChange')) {
-        $('#enter_site').modal('hide');
-        $('#adressChange').modal('show');
-      } else {
-        if (data == 2) {
-          location.reload(true);
+      console.log(data);
+
+      if (data['name'] == '' || data['name'] == null) {
+        if ($('#adressChange')) {
+          $('#enter_site').modal('hide');
+          $('#adressChange').modal('show');
         }
+      } else {
+        location.reload(true);
       }
     },
     error: function error(xhr, status, _error4) {
@@ -21731,7 +21777,7 @@ $('.add-product-secondary .pic-item').on('click', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! e:\OpenServer\domains\fason.loc\resources\js\main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! /home/shuhrat/Desktop/Актуальные проекты/fason.tj/resources/js/main.js */"./resources/js/main.js");
 
 
 /***/ })
