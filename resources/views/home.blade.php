ef8c1952614bd5588806bde1a605ab50e539375d
Shuhrat-web <shuhrat.safoev@colibri.tj>
2021-07-09 16:06:04 +0500

    cards fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,7 +149,7 @@
             <div class="col d-flex align-items-center justify-content-center mb-4 px-1 px-md-2">
                 <div class="card rounded shadow border-0 h-100 w-100">
                     <img class="img-fluid rounded" src="{{ Storage::url($product->image) ?? '/storage/app/public/theme/no-photo.jpg' }}" alt="">
-                    <div class="container">
+                    <div class="container d-flex flex-column justify-content-between flex-wrap">
                         <h4 class="product-name shop-subject mt-3" style="height: 2rem;">{{ Str::limit($product->name, 26) }}</h4>
                         <div class="price-place d-flex justify-content-between align-items-center my-3 text-danger">
                             <span class="font-weight-bold">
@@ -177,7 +177,7 @@
             <div class="col d-flex align-items-center justify-content-center mb-4 px-1 px-md-2">
                 <div class="card rounded shadow border-0 h-100 w-100">
                     <img class="img-fluid rounded" src="{{ Storage::url($product->image) ?? '/storage/app/public/theme/no-photo.jpg' }}" alt="">
-                    <div class="container">
+                    <div class="container d-flex flex-column justify-content-between flex-wrap">
                         <h4 class="product-name shop-subject mt-3" style="height: 2rem;">{{ Str::limit($product->name, 26) }}</h4>
                         <div class="price-place d-flex justify-content-between align-items-center my-3  text-danger">
                             <span class="font-weight-bold">
