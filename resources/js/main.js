fbb4d658bf59ffa4b3bb648b609ba25182f2b874
Firuz Shirinov <firuz.shirinov@colibri.tj>
2021-08-08 12:05:35 +0500

    npm run prod

--- resources/js/main.js
+++ resources/js/main.js
@@ -201,37 +201,36 @@ $(document).ready(function() {
     })
 
     $('.sms--false').hide()
-    // $(window).scroll(fetchPosts)
+    $(window).scroll(fetchPosts)
 
-    // var old_page = '';
-    // function fetchPosts() {
-    //     // var url = $(location).attr('href')
-    //     var page = $('.endless-pagination').data('next-page')
-    //     if (page !== null && page !== '' && !page.substring(0, 5).includes('null&')) {
-    //         clearTimeout($.data(this, 'scrollCheck'))
+    var old_page = '';
+    function fetchPosts() {
+        var page = $('.endless-pagination').data('next-page')
+        if (page !== null && page !== '' && !page.substring(0, 5).includes('null&')) {
+            $('#scroll-spinner').removeClass('d-none')
+            // clearTimeout($.data(this, 'scrollCheck'))
 
-    //         $.data(
-    //             this,
-    //             'scrollCheck',
-    //             setTimeout(function() {
-    //                 $('#scroll-spinner').removeClass('d-none')
-
-    //                 var scroll_position_for_posts_load = $(window).height() + $(window).scrollTop() + 500
-    //                 if (scroll_position_for_posts_load >= $(document).height()) {
-    //                     var style = $('#catProducts .row').attr('data-style')
-    //                     if(old_page != page){
-    //                         $.get(page, { style: style }, function(data) {
-    //                             $('.endless-pagination').append(data.posts)
-    //                             $('.endless-pagination').data('next-page', data.next_page)
-    //                             $('#scroll-spinner').addClass('d-none')
-    //                         })
-    //                         old_page = page;
-    //                     }
-    //                 }
-    //             }, 200)
-    //         )
-    //     }
-    // }
+            // $.data(
+            //     this,
+            //     'scrollCheck',
+                setTimeout(function() {
+                    // $('#scroll-spinner').removeClass('d-none')
+                    var scroll_position_for_posts_load = $(window).height() + $(window).scrollTop() + 500
+                    if (scroll_position_for_posts_load >= $(document).height()) {
+                        var style = $('#catProducts .row').attr('data-style')
+                        if(old_page != page){
+                            $.get(page, { style: style }, function(data) {
+                                $('.endless-pagination').append(data.posts)
+                                $('.endless-pagination').data('next-page', data.next_page)
+                                $('#scroll-spinner').addClass('d-none')
+                            })
+                            old_page = page;
+                        }
+                    }
+                }, 200)
+            // )
+        }
+    }
     $('.markets').slick({
         dots: true,
         infinite: false,
