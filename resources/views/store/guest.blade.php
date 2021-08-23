79b8a131d95dbc13c2d0e08d865b37e19baa86b9
Rahmatshoh <rahmatshoh.rahmonov@mail.ru>
2021-08-23 15:32:07 +0500

    fix banners inside the store

--- resources/views/store/guest.blade.php
+++ resources/views/store/guest.blade.php
@@ -22,18 +22,18 @@
            </div>
          </div>
          <div class="col-md-9 px-0 px-md-2 position-relative">
-          <img src="/storage/{{$store->cover ?? '/theme/banner_store.svg' }}" class="w-100 rounded store-banner object-contain" height="215">
+          <img src="/storage/{{$store->cover ?? '/theme/banner_store.svg' }}" class="w-100 rounded store-banner object-contain">
          </div>
-         <div class="col-12 d-block my-4">
+         <div class="col-12 d-block mb-4">
           <div class="row align-items-center">
             <div class="col-4 col-md-3 text-center">
-              <div class="d-inline-block position-relative  d-md-none">
-                <img class="store-icon object-cover" src="/storage/{{$store->avatar ?? '/theme/avatar_store.svg' }}" width="80px" height="80px">
+              <div class="d-inline-block position-relative  d-md-none mt-n4">
+                <img class="store-icon object-cover fs_border" src="/storage/{{$store->avatar ?? '/theme/avatar_store.svg' }}" width="80px" height="80px">
               </div>
             </div>
-            <div class="col-8 col-md-9 order-0 order-lg-1 mt-0 mt-sm-3">
+            <div class="col-8 col-md-9 order-0 order-lg-1 mt-2 mt-sm-3">
               <div class="d-flex flex-column flex-md-row justify-content-between">
-                <h3 class="font-weight-bold">{{ $store->name }}</h3>
+                <h4 class="font-weight-bold">{{ $store->name }}</h4>
                 <span class="text-monospace">г.{{ $store->city->name ?? ''}}</span>
               </div>
             </div>
@@ -42,7 +42,7 @@
       </div>
       <!--Edit logo and banner end-->
       <!--store info start-->
-      <div class="row align-items-center my-4">
+      <div class="row align-items-center mb-4">
         <div class="col-12 col-md-3">
           <h3 class="font-weight-bold">О магазине:</h3>
         </div>
@@ -56,8 +56,8 @@
     </div>
     <!--goods start-->
     <div class="container mb-5 px-md-0">
-      <h2 class="mb-4 mt-2 my-md-4 text-muted mb-other-product font-weight-bold text-xs-center">Все товары магазина</h2>
-      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 my-3 px-2 px-md-0 pb-3 px-md-2">
+      <h2 class="mb-4 mt-2 my-md-4 text-muted mb-other-product font-weight-bold">Все товары магазина</h2>
+      <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 my-3 px-3 px-md-3 pb-3 px-md-2">
 
         @forelse ($products as $product)
         <div class="col d-flex align-items-center justify-content-center mb-4 px-1 px-md-2">
